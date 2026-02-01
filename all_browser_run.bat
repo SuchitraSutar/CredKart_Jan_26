affecfe9910dd1db14fc2c91e5f955e36a9f9bb5
SuchitraSutar <suchitra.sutar94@gmail.com>
2026-02-01 16:16:17 +0530

    git edited

---
+++ all_browser_run.bat
@@ -0,0 +1,3 @@
+pytest -n=auto --html=Html_reports\my_chromereport_1st_feb_2026.html --browser chrome --alluredir=AllureReports --disable-warnings
+pytest -n=auto --html=Html_reports\my_firefoxreport_1st_feb_2026.html --browser firefox --alluredir=AllureReports --disable-warnings
+pytest -n=auto --html=Html_reports\my_edgereport_1st_feb_2026.html --browser edge --alluredir=AllureReports --disable-warnings
